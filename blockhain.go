e8e859c5ef2ef1cef600869b78651039f709565e
neha-decrypt <neha.decrypt@gmail.com>
2023-09-19 18:09:07 +0530

    added blockchain struct

--- blockhain.go
+++ blockhain.go
@@ -1,14 +1,63 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 )
+
+type Block struct{
+	nonce int;
+	previousHash string;
+	timestamp int64;
+	transactions []string;
+}
+
+type Blockchain struct{
+	transactionPool []string;
+	chain []*Block;
+}
+
+func (b *Block) Print(){
+	fmt.Printf("Nonce          %d\n",b.nonce)
+	fmt.Printf("Timestamp      %d\n",b.timestamp)
+	fmt.Printf("Previous_Hash  %s\n",b.previousHash)
+	fmt.Printf("Transactions   %d\n",b.transactions)
+}
+
+func (bc *Blockchain) Print(){
+	fmt.Println(bc)
+}
+
 
 func init(){
 	log.SetPrefix("Blockchain: ")
 }
 
+func newBlockchain() *Blockchain{
+bc:=new(Blockchain)
+bc.createBlock(0,"init hash")
+return bc
+}
+
+func newBlock(nonce int,previousHash string) *Block{
+b:=new(Block)
+b.timestamp=time.Now().UnixNano()
+b.previousHash=previousHash
+b.nonce=nonce
+return b
+}
+
+func (bc *Blockchain) createBlock(nonce int, previousHash string) *Block{
+	b:=newBlock(nonce,previousHash)
+	bc.chain = append(bc.chain,b)
+	return b
+}
+
 func main(){
-	a:="test"
-	log.Println(a)
+	bc:=newBlockchain()
+	bc.createBlock(1,"hash1")
+	bc.createBlock(2,"hash2")
+	bc.createBlock(3,"hash3")
+	bc.Print()
 }
