31b1eaf94ddc2f1faaa75ff6ec1f781ba4cbb09a
neha-decrypt <neha.decrypt@gmail.com>
2023-09-28 11:13:22 +0530

    checking if port active

---
+++ utils/neighbor.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+func IsFoundHost(host string, port uint16) bool {
+	target := fmt.Sprintf("%s:%d", host, port)
+	_, err := net.DialTimeout("tcp", target, 1*time.Second)
+	if err != nil {
+		fmt.Printf("%s,%v\n", target, err)
+		return false
+	}
+	return true
+}
